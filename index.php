6cf624b788cc78fcc8972afa86e79d257dfb01bf
farhaner <farhaner23@gmail.com>
2024-09-28 14:26:47 +0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Mitra Keluarga</title>
+</head>
+
+<body class="bg-dark">
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top border-bottom">
+        <div class="container">
+            <a href="#"><img class="navbar-logo" src="img/Logo-Mitra-Keluarga.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup center">
+                <div class="navbar-nav">
+                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#about">About</a>
+                    <a class="nav-link" href="#contact">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <section class="hero position-relative" id="home">
+        <div class="overlay position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-50"></div>
+        <main class="content d-flex flex-column align-items-center justify-content-center text-center" style="min-height: 100vh;">
+            <div class="col-md-5 bg-white p-3 rounded shadow mt-3">
+                <h1 class="fw-bold">Jaga Kebersihan, Cegah Infeksi!</h1>
+                <h6 class="text-dark">Sampah medis merupakan masalah serius yang perlu mendapat perhatian serius. Limbah dari rumah sakit, klinik, dan fasilitas kesehatan lainnya ini mengandung berbagai macam zat berbahaya yang dapat mencemari lingkungan dan mengancam kesehatan manusia.</h6>
+            </div>
+        </main>
+    </section>
+
+
+    <section class="about" id="about">
+        <div class=" text-center text-white">
+            <h2><span>Tentang</span>Kami</h2>
+            <div class="img mt-5">
+                <img class="infeksius-logo rounded-circle" src="img/sampah infeksius.jpg" alt="">
+            </div>
+            <div class="row justify-content-center mt-3">
+                <div class="col-md-3 fw-light">
+                    <p>Sampah medis merupakan masalah serius yang perlu mendapat perhatian serius. Sampah medis harus dikelola dengan sangat hati-hati, karena berisiko tinggi bagi kesehatan manusia dan lingkungan.</p>
+                </div>
+                <div class="col-md-3 fw-light">
+                    <p>Pengelolaan yang tidak tepat dapat menyebabkan penyebaran penyakit, pencemaran lingkungan, dan bahaya keselamatan bagi para pekerja kesehatan maupun masyarakat umum.</p>
+                </div>
+                <div class="col-md-3 fw-light">
+                    <p> Oleh karena itu, sampah medis sering kali harus diolah dengan cara yang khusus, seperti insinerasi, autoklaf, atau penanganan limbah berbahaya sesuai dengan peraturan kesehatan yang berlaku.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="contact " id="contact">
+        <div class=" text-center text-white">
+            <h2><span>Our</span>Contact</h2>
+            <footer class="mt-5">
+                <a href="https://api.whatsapp.com/send?phone=62895321920003&text=Hai,%20bisakah%20saya%20bertanya%20tentang%20sampah%20medis%20?" target="_blank"><i class="bi bi-whatsapp text-white"> Muhammad Farhan Erlangga |</i></a>
+                <a href="https://api.whatsapp.com/send?phone=6282123345226&text=Hai,%20bisakah%20saya%20bertanya%20tentang%20sampah%20medis%20?"><i class=" bi bi-whatsapp text-white"> Muhammad Haris Syawaludin |</i></a>
+                <a href=""><i class="bi bi-whatsapp text-white"> |</i></a>
+                <a href=""><i class="bi bi-whatsapp text-white"> |</i></a>
+                <p>Website created by <a href="" class="text-decoration-none">farhan erlangga</a></p>
+            </footer>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
